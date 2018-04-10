117c25121f49d5f06db7bca4277891a5c6574665
unknown <vanessa.kovalsky@free.fr>
2018-04-10 12:23:29 +0200

    add server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const { parse } = require('querystring');
+
+function collectRequestData(request, callback){
+  const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+
+  if(request.headers['content-type'] === FORM_URLENCODED) {
+    let body ='';
+    request.on('data', chunk => {
+      body += chunk.toString();
+    });
+    request.on('end', () => {
+      callback(parse(body));
+    });
+  }
+  else {
+    callback(null);
+  }
+}
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST'){
+    collectRequestData(req, result => {
+      console.log(result);
+      res.end(`Le repertoire d'origine est ${result.originPath}`);
+    });
+  }
+  else{
+    res.end(`
+      <!doctype html>
+      <html>
+      <body>
+        <form ation="/" method="post">
+          <input type="text" name="originPath" /><br />
+          <input type="text" name="destPath" /><br />
+          <button>Save</button>
+        </form>
+      </body>
+      </html>
+    `);
+  }
+});
+
+server.listen(3000);
