bcc98cec90a6479d96ff43897a15bebc28bae013
Vanessa DAVID <vanessa.david@alten.com>
2018-04-12 17:37:48 +0200

    Ajout tous les TP

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const { parse } = require('querystring');
+var read_files = require('readfiles');
 
 function collectRequestData(request, callback){
   const FORM_URLENCODED = 'application/x-www-form-urlencoded';
@@ -22,6 +23,8 @@ const server = http.createServer((req, res) => {
   if (req.method === 'POST'){
     collectRequestData(req, result => {
       console.log(result);
+      console.log(read_files);
+      //read_files.readFiles(result.originPath, result.destPath);
       res.end(`Le repertoire d'origine est ${result.originPath}`);
     });
   }
